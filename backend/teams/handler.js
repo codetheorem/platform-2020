d455ec71bbd590ae648e57f5a2d580dda5092809
Maosef <pazzun@gmail.com>
2020-06-17 21:33:26 -0400

    add inbite_to_team

--- backend/teams/handler.js
+++ backend/teams/handler.js
@@ -2,7 +2,8 @@ const db = require("simple-dynamodb");
 const AWS = require('aws-sdk');
 const uuid = require('uuid');
 
-const TABLE_NAME = 'platform-teams-dev';
+const TEAMS_TABLE_NAME = 'platform-teams-dev';
+const INVITES_TABLE_NAME = 'platform-team-invites';
 
 
 module.exports.create_team = async event => {
@@ -10,7 +11,33 @@ module.exports.create_team = async event => {
   const ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
 
   const params = {
-    TableName: TABLE_NAME,
+    TableName: TEAMS_TABLE_NAME,
+    Item: {
+      'id': {S:uuid.v4()},
+    },
+  };
+
+  // dynamically add post request body params to document
+  Object.keys(body).forEach(k => {
+    params.Item[k] = {S: body[k]}
+  });
+
+  // Call DynamoDB to add the item to the table
+  const result = await ddb.putItem(params).promise()
+
+  // return 500 on error
+  return {
+    statusCode: 200,
+    body: JSON.stringify(result),
+  };
+};
+
+module.exports.invite_to_team = async event => {
+  const body = JSON.parse(event.body);
+  const ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+
+  const params = {
+    TableName: INVITES_TABLE_NAME,
     Item: {
       'id': {S:uuid.v4()},
     },
