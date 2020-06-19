e82c53c0347c8c8dc72ce766ab50e5ea8c737274
Maosef <pazzun@gmail.com>
2020-06-18 22:39:30 -0400

    create leave_team function

--- backend/teams/handler.js
+++ backend/teams/handler.js
@@ -4,14 +4,14 @@ const uuid = require('uuid');
 
 const TEAMS_TABLE_NAME = 'platform-teams-dev';
 const INVITES_TABLE_NAME = 'platform-team-invites';
+const MEMBERSHIPS_TABLE_NAME = 'platform-team-memberships';
 
-
-module.exports.create_team = async event => {
+async function post_body_to_table(event, table_name) {
   const body = JSON.parse(event.body);
   const ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
 
   const params = {
-    TableName: TEAMS_TABLE_NAME,
+    TableName: table_name,
     Item: {
       'id': {S:uuid.v4()},
     },
@@ -32,29 +32,35 @@ module.exports.create_team = async event => {
   };
 };
 
+module.exports.create_team = async event => {
+  return post_body_to_table(event, TEAMS_TABLE_NAME);
+};
+
 module.exports.invite_to_team = async event => {
+  return post_body_to_table(event, INVITES_TABLE_NAME);
+};
+
+module.exports.join_team = async event => {
+  return post_body_to_table(event, MEMBERSHIPS_TABLE_NAME);
+};
+
+module.exports.leave_team = async event => {
   const body = JSON.parse(event.body);
   const ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
 
-  const params = {
-    TableName: INVITES_TABLE_NAME,
-    Item: {
-      'id': {S:uuid.v4()},
+  // Call DynamoDB to delete the item
+  const delete_params = {
+    TableName: MEMBERSHIPS_TABLE_NAME,
+    Key: {
+      'id': {S: body['id']},
     },
   };
-
-  // dynamically add post request body params to document
-  Object.keys(body).forEach(k => {
-    params.Item[k] = {S: body[k]}
-  });
-
-  // Call DynamoDB to add the item to the table
-  const result = await ddb.putItem(params).promise()
+  
+  const status_result = await ddb.deleteItem(delete_params).promise();
 
   // return 500 on error
   return {
     statusCode: 200,
-    body: JSON.stringify(result),
+    body: JSON.stringify(status_result),
   };
-
 };
