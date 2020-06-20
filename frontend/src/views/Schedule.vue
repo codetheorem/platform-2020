17d3c4f74c32b16c45aa16add91952d84b9438c0
Hugo Burbelo <hburbelo@phone2action.com>
2020-06-20 14:38:24 -0400

    Dynamically render schedule

--- frontend/src/views/Schedule.vue
+++ frontend/src/views/Schedule.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="about">
     <h1>This is a schedule page</h1>
+    <div>
+      <div v-for="event in schedule" :key="event.id">
+        <span>{{ event.name}}</span>
+      </div>
+    </div>
   </div>
 </template>
 
