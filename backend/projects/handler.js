9e4fb28e922384c9c8cdd53981e2914ef51d15a1
Maosef <pazzun@gmail.com>
2020-06-27 14:04:28 -0400

    create functions for getting and creating sponsors

--- backend/projects/handler.js
+++ backend/projects/handler.js
@@ -1,18 +1,59 @@
-'use strict';
+const AWS = require('aws-sdk');
+const UUID = require('uuid');
 
-module.exports.hello = async event => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
+AWS.config.update({region:'us-east-1'});
+
+
+// Retrieves all sponsors from the database
+module.exports.get_sponsorship_info = async event => {
+  const params = {
+    TableName: process.env.SPONSORS_INFO_TABLE,
   };
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+  const ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+
+  const result = await ddb.scan(params).promise();
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(result.Items),
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true
+    }
+  };
+};
+
+
+// Adds a new sponsor to the database
+module.exports.add_sponsor = async event => {
+  const body = JSON.parse(event.body);
+
+  const ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+
+  const id = UUID.v4();
+
+  const params = {
+    TableName: process.env.SPONSORS_INFO_TABLE,
+    Item: {}
+  };
+
+  body.id = id;
+
+  // dynamically add post request body params to document
+  Object.keys(body).forEach(k => {
+    params.Item[k] = {S: body[k]}
+  });
+
+  // Call DynamoDB to add the item to the table
+  const result = await ddb.putItem(params).promise();
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(result.Item),
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
+    }
+  };
 };
