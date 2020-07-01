925e34a04b7af838ca30a6036c3b19cb35892f1b
Hugo Burbelo <hburbelo@phone2action.com>
2020-07-01 15:29:50 -0400

    Add background gradient

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="about">
+  <div class="login-container">
     <h1>This is a login page</h1>
     <Button size="lg" text="Hugo is cool"/>
   </div>
@@ -12,6 +12,15 @@ export default {
   name: 'Login',
   components: {
     Button
-  },
+  }
 };
 </script>
+
+
+<style>
+  #app {
+    background-image: radial-gradient(#ffafbd, #ffc3a0);
+    width: 100vw;
+    height: 100vh;
+  }
+</style>
