7f7c15269bf9b25f0e46b203990b55b54204df42
Owen Luo <luoowen@gmail.com>
2020-07-11 13:16:36 -0400

    added redirect on livestream navbar click (#45)
    * added url redirect to technica yt when livestream navbutton is clicked
    
    * changed router so it won't navigate to livestream blank page
    
    * Remove empty line
    
    Co-authored-by: Hugo Burbelo <hburbelo@phone2action.com>

--- frontend/src/config/general.js
+++ frontend/src/config/general.js
@@ -3,7 +3,8 @@ export default {
     SCHEDULE_BASE_ENDPOINT: 'https://dxz39cp5d0.execute-api.us-east-1.amazonaws.com',
     SAMPLE_USER_ID: 'SAMPLE_USER_ID',
     SIGNUP_FORM_LINK: 'https://gotechnica.org',
-    USERS_BASE_ENDPOINT: 'https://xgyrlul4tk.execute-api.us-east-1.amazonaws.com'
+    USERS_BASE_ENDPOINT: 'https://xgyrlul4tk.execute-api.us-east-1.amazonaws.com',
+    LIVESTREAM_LINK: 'https://www.youtube.com/channel/UC-Wo4UqhGUKQEp4o-51VMIw'
   },
   testing: {
     SCHEDULE_BASE_ENDPOINT: 'https://dxz39cp5d0.execute-api.us-east-1.amazonaws.com',
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '../views/Home.vue';
+import Config from "../config/general";
 
 Vue.use(VueRouter);
 
@@ -21,7 +22,10 @@ const routes = [
     path: '/live-stream',
     name: 'Live Stream',
     component: () => import('../views/LiveStream.vue'),
-    displayInNavBar: true
+    displayInNavBar: true,
+    beforeEnter() {
+      window.open(Config.dev.LIVESTREAM_LINK, "_blank");
+    }
   },
   {
     path: '/schedule',
@@ -60,17 +64,17 @@ const routes = [
     displayInNavBar: false
   },
   {
-    path: '/authenticate', 
+    path: '/authenticate',
     component: () => import('../views/Authenticate.vue'),
     displayInNavBar: false
   },
   {
-    path: '/logout', 
+    path: '/logout',
     component: () => import('../views/Logout.vue'),
     displayInNavBar: false
   },
   {
-    path: '/:shortlink', 
+    path: '/:shortlink',
     component: () => import('../views/ShortLink.vue'),
     displayInNavBar: false
   }
