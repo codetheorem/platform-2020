f33bdcbaf466c41752dea2f1d74bf5b1e77f2108
Andrew Mao <pazzun@gmail.com>
2020-07-21 08:39:19 -0400

    Mentorship endpoints (#65)
    * begin mentorship endpoints
    
    * create tests

--- backend/projects/handler.js
+++ backend/projects/handler.js
@@ -25,7 +25,6 @@ module.exports.get_sponsorship_info = withSentry(async event => {
   };
 });
 
-
 // Adds a new sponsor to the database
 module.exports.add_sponsor = withSentry(async event => {
   const body = JSON.parse(event.body);
@@ -93,6 +92,104 @@ module.exports.create_mentorship_request = withSentry(async request => {
   return {
     statusCode: 200,
     body: JSON.stringify(params.Item),
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true
+    }
+  };
+});
+
+// Retrieves all active mentorship requests from the database
+module.exports.get_active_mentorship_requests = withSentry(async event => {
+  const params = {
+    TableName: process.env.MENTORSHIP_REQUESTS_TABLE,
+    KeyConditionExpression: "active = true",
+  };
+
+  const ddb = new AWS.DynamoDB.DocumentClient();
+  const result = await ddb.scan(params).promise();
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(result.Items),
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true
+    }
+  };
+});
+
+// update a mentorship request
+module.exports.update_mentorship_request = withSentry(async event => {
+
+  const body = JSON.parse(event.body);
+
+  // dynamically add body params to update request 
+  // this assumes body params are flat, AKA no dicts/lists inside
+  // use an attribute map to avoid a name conflict with reserved keywords
+  // atm only supports 52 attribute mappings
+  let update_expression = "set ";
+  let attribute_map = {};
+  let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+  let cur_char_index = 0;
+  let charactersLength = characters.length;
+
+  Object.keys(body).forEach(k => {
+
+    if (k != 'id') {
+      if (cur_char_index < charactersLength) {
+        let attr = characters.charAt(cur_char_index);
+        cur_char_index += 1;
+        attribute_map[`:${attr}`] = body[k];
+        update_expression += `${k} = :${attr}, `;
+      } else {
+        throw "attribute placeholders exhausted (num of attributes is >52).";
+      }
+    }
+  });
+  update_expression = update_expression.slice(0, -2);;
+
+  const params = {
+    TableName: process.env.MENTORSHIP_REQUESTS_TABLE,
+    Key:{
+      "id": body['id'],
+    },
+    UpdateExpression: update_expression,
+    ExpressionAttributeValues: attribute_map,
+    ReturnValues:"UPDATED_NEW" // return updated attributes
+  };
+
+  const ddb = new AWS.DynamoDB.DocumentClient();
+
+  const result = await ddb.update(params).promise();
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(result.Items),
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true
+    }
+  };
+});
+
+// Retrieves mentorship requests by a user from the database
+module.exports.get_user_mentorship_requests = withSentry(async event => {
+
+  let user_id = event.queryStringParameters.user_id;
+
+  const params = {
+    TableName: process.env.MENTORSHIP_REQUESTS_TABLE,
+    KeyConditionExpression: `user_id = ${user_id}`,
+  };
+
+  const ddb = new AWS.DynamoDB.DocumentClient();
+
+  const result = await ddb.scan(params).promise();
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(result.Items),
     headers: {
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Credentials': true
