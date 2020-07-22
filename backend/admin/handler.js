d7d22e1d05bc9702975bfd742c2c30a28550af4d
Hugo Burbelo <hburbelo@phone2action.com>
2020-07-22 09:30:40 -0400

    Cleanup

--- backend/admin/handler.js
+++ backend/admin/handler.js
@@ -20,4 +20,4 @@ module.exports.hello = withSentry(async event => {
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-});
+}); 
