7f33976782f7906eb81f1b4085d2d05f180091ce
Hugo Burbelo <hburbelo@phone2action.com>
2020-08-23 19:56:00 -0400

    Update frontend prod config to use correct teams and users base endpoints

--- frontend/src/config/general.js
+++ frontend/src/config/general.js
@@ -34,9 +34,9 @@ export default {
     SCHEDULE_BASE_ENDPOINT: 'https://n1zcgon3ub.execute-api.us-east-1.amazonaws.com',
     SAMPLE_USER_ID: 'SAMPLE_USER_ID',
     SIGNUP_FORM_LINK: 'https://gotechnica.org',
-    USERS_BASE_ENDPOINT: 'https://xhy30yrm0f.execute-api.us-east-1.amazonaws.com',
+    USERS_BASE_ENDPOINT: 'https://nc0gm7vx1i.execute-api.us-east-1.amazonaws.com',
     LIVESTREAM_LINK: 'https://www.youtube.com/channel/UC-Wo4UqhGUKQEp4o-51VMIw',
     SPONSORSHIP_LINK: 'https://www.vfairs.com/',
-    TEAMS_BASE_ENDPOINT: 'https://e2hezie6kb.execute-api.us-east-1.amazonaws.com',
+    TEAMS_BASE_ENDPOINT: ' https://v0usve1q3f.execute-api.us-east-1.amazonaws.com',
   },
 };
