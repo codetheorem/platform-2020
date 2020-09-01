bdcd0b439afe5224517e5e6640a2c4d2310f59a7
Hugo Burbelo <hburbelo@phone2action.com>
2020-09-01 11:47:01 -0400

    Begin adding live stream

--- frontend/src/views/LiveStream.vue
+++ frontend/src/views/LiveStream.vue
@@ -1,5 +1,31 @@
 <template>
-  <div class="about">
-    <h1>This is a live stream page</h1>
+  <div class="page-container">
+    <img src="@/assets/livestream.png" style="width: 100%; height: 100%;">
+    <iframe id="live-stream-wrapper" src="https://www.youtube.com/embed/5jPwFWIvBzk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
   </div>
 </template>
+
+<script>
+import generalMixin from '../mixins/general';
+
+export default {
+  name: 'LiveStream',
+  mixins: [generalMixin],
+  methods: {
+    goBack() {
+      this.$router.push({ name: 'Home' });
+    },
+  },
+};
+</script>
+
+<style scoped>
+  .page-container {
+    width: 100vw;
+    height: 90vh;
+  }
+  #live-stream-wrapper {
+    width: 30vw;
+    height: 20vh;
+  }
+</style>
