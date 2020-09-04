b3de914d2ae3ce1195873d93f2a9b55bbb5a9ca1
Hugo Burbelo <hburbelo@phone2action.com>
2020-09-03 20:25:05 -0400

    Fix sponsor endpoint config

--- frontend/src/config/general.js
+++ frontend/src/config/general.js
@@ -4,7 +4,7 @@ export default {
     SAMPLE_USER_ID: 'SAMPLE_USER_ID',
     SIGNUP_FORM_LINK: 'https://gotechnica.org',
     USERS_BASE_ENDPOINT: 'https://xgyrlul4tk.execute-api.us-east-1.amazonaws.com',
-    SPONSORS_INFO_ENDPOINT: 'https://8cefcll545.execute-api.us-east-1.amazonaws.com',
+    SPONSORS_INFO_ENDPOINT: 'https://4yngdno31k.execute-api.us-east-1.amazonaws.com',
     LIVESTREAM_LINK: 'https://www.youtube.com/channel/UC-Wo4UqhGUKQEp4o-51VMIw',
     SPONSORSHIP_LINK: 'https://www.vfairs.com/',
     TEAMS_BASE_ENDPOINT: 'https://e2hezie6kb.execute-api.us-east-1.amazonaws.com',
