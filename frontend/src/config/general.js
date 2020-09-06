2a7eacc3e41464d2268fe24caabf1412acb520e4
Hugo Burbelo <hburbelo@phone2action.com>
2020-09-05 21:06:56 -0400

    Fix users dev endpoint

--- frontend/src/config/general.js
+++ frontend/src/config/general.js
@@ -3,7 +3,7 @@ export default {
     SCHEDULE_BASE_ENDPOINT: 'https://dxz39cp5d0.execute-api.us-east-1.amazonaws.com',
     SAMPLE_USER_ID: 'SAMPLE_USER_ID',
     SIGNUP_FORM_LINK: 'https://gotechnica.org',
-    USERS_BASE_ENDPOINT: 'https://xgyrlul4tk.execute-api.us-east-1.amazonaws.com',
+    USERS_BASE_ENDPOINT: 'https://9zxvldglh9.execute-api.us-east-1.amazonaws.com',
     SPONSORS_INFO_ENDPOINT: 'https://4yngdno31k.execute-api.us-east-1.amazonaws.com',
     LIVESTREAM_LINK: 'https://www.youtube.com/channel/UC-Wo4UqhGUKQEp4o-51VMIw',
     SPONSORSHIP_LINK: 'https://www.vfairs.com/',
