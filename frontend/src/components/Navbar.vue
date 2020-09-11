ff6d71fb33194e367bb9a18ecfbde57cd655e8ce
Maosef <pazzun@gmail.com>
2020-09-10 20:02:59 -0400

    make navbar sticky

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light navigation-box">
+  <div class="sticky-top">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light navigation-box"">
       <div class="logo-nav">
         <router-link to="/">
           <a class="navbar-brand" href="#">
@@ -61,6 +61,7 @@ export default {
     };
   },
 };
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
