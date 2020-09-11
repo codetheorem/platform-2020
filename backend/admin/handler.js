2cd1ac5a17ffbccbfdbf35ed6eabc59ffed1ff39
Samuel Howard <samuelhowardstudent@gmail.com>
2020-09-11 16:20:23 -0400

    Adds track_user_activity backend

--- backend/admin/handler.js
+++ backend/admin/handler.js
@@ -1,16 +1,42 @@
 const AWS = require('aws-sdk');
+const UUID = require('uuid');
 const withSentry = require('serverless-sentry-lib');
 
 AWS.config.update({ region: 'us-east-1' });
 
-module.exports.hello = withSentry(async (event) => ({
-  statusCode: 200,
-  body: JSON.stringify(
-    {
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
+// Posts activity data to the activity table for tracking purposes
+module.exports.track_user_activity = withSentry(async (action) => {
+  const body = JSON.parse(action.body);
+  const ddb = new AWS.DynamoDB.DocumentClient();
+
+  const activityId = UUID.v4();
+
+  if (!body.user_id || !body.action) {
+    return {
+      statusCode: 500,
+      body: 'track_user_activity is missing user_id or action',
+    };
+  }
+
+  const params = {
+    TableName: process.env.ACTIVITY_TABLE,
+    Item: {
+      id: activityId,
+      user_id: body.user_id,
+      event: body.action,
+      timestamp: new Date().toString(),
     },
-    null,
-    2,
-  ),
-}));
+
+  };
+
+  const result = await ddb.scan(params).promise();
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(result.Items),
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
+    },
+  };
+});
