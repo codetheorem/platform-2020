0da57f64f970915b1d4109e1db69e6bd40ae82bf
Hugo Burbelo <hburbelo@phone2action.com>
2020-09-12 19:57:32 -0400

    Add live stream embedded

--- frontend/src/views/LiveStream.vue
+++ frontend/src/views/LiveStream.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="page-container">
     <img src="@/assets/livestream.png" style="width: 100%; height: 100%;">
-    <iframe id="live-stream-wrapper" src="https://www.youtube.com/embed/5jPwFWIvBzk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <div id="iframe-wrapper">
+      <iframe id="live-stream-wrapper" :src="liveStreamLink" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
   </div>
 </template>
 
 <script>
 import generalMixin from '../mixins/general';
+import Config from '../config/general';
 
 export default {
   name: 'LiveStream',
@@ -16,16 +19,39 @@ export default {
       this.$router.push({ name: 'Home' });
     },
   },
+  computed: {
+    liveStreamLink() {
+      return Config.shared.LIVESTREAM_LINK;
+    },
+  },
 };
 </script>
 
 <style scoped>
+  .live-stream-background {
+    z-index: 188;
+  }
   .page-container {
     width: 100vw;
     height: 90vh;
+    display: flex;
+    align-content: center;
+    justify-content: center;
+  }
+  #iframe-wrapper {
+    width: 70vw;
+    height: 60vh;
+    position: absolute;
+    align-self: center;
+    z-index: 1000;
+    border: 2rem solid var(--lighter-purple);
+    padding: 0;
+    padding-top: 0px;
+    margin-top: -1px;
   }
   #live-stream-wrapper {
-    width: 30vw;
-    height: 20vh;
+    width: 100.25%;
+    height: 100%;
+    margin-bottom: -1px;
   }
 </style>
