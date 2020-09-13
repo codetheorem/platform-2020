229790b96949444887b6b922f3e1efa4e04190b8
Samuel Howard <samuelhowardstudent@gmail.com>
2020-09-13 19:08:10 -0400

    Adds track_user_activity backend endpoint

--- backend/users/handler.js
+++ backend/users/handler.js
@@ -546,3 +546,39 @@ module.exports.update_hacker_profile = withSentry(async event =>{
     }
   };
 });
+
+// Posts activity data to the activity table for tracking purposes
+module.exports.track_user_activity = withSentry(async (action) => {
+  const body = JSON.parse(action.body);
+  const ddb = new AWS.DynamoDB.DocumentClient();
+
+  const activityId = UUID.v4();
+
+  if (!body.user_id || !body.action) {
+    return {
+      statusCode: 500,
+      body: 'track_user_activity is missing user_id or action',
+    };
+  }
+
+  const params = {
+    TableName: process.env.ACTIVITY_TABLE,
+    Item: {
+      id: activityId,
+      user_id: body.user_id,
+      event: body.action,
+      timestamp: new Date().toString(),
+    },
+  };
+
+  await ddb.put(params).promise();
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(params.Item),
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
+    },
+  };
+});
