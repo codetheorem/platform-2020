3d2fdad6e44c2095f30de0d5aeb2957bf6aa7a82
Hugo Burbelo <hburbelo@phone2action.com>
2020-09-19 21:01:10 -0400

    Make progress on display

--- frontend/src/views/Schedule.vue
+++ frontend/src/views/Schedule.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="schedule-page">
     <h2 class="page-header">Events</h2>
-    <div class="schedule-list">
+    <div v-if="dataLoaded" class="schedule-list">
       <div class="schedule-list-title">
-        <span v-for="day in days" :key="day" class="schedule-list-title-item" :class="{'schedule-list-title-item-selected': day === selectedDay}" @click="selectTitleItem(day)">{{ day.toUpperCase() }}</span>
+        <span v-for="day in days" :key="getDayOfTheWeek(day)" class="schedule-list-title-item" :class="{'schedule-list-title-item-selected': day === selectedDay}" @click="selectTitleItem(day)">{{ getDayOfTheWeek(day).toUpperCase() }}</span>
       </div>
       <div id="schedule-body" class="schedule-body">
         <div class="schedule-time">
@@ -14,7 +14,14 @@
         <div class="schedule-content">
           <div v-for="scheduleColumn in scheduleColumns" :key="scheduleColumn" class="schedule-column">
               <div v-for="timeWindow in timeWindows" :key="timeWindow" class="timewindow">
-                <div class="schedule-content-item">{{ timeWindow }}</div>
+                <div v-if="formattedEvents[selectedDay][timeWindow].length >= scheduleColumn" class="schedule-content-item" :class="formattedEvents[selectedDay][timeWindow][scheduleColumn - 1].branding.class">
+                  <div class="schedule-content-item-star">
+                    <img :src="getImgUrl(formattedEvents[selectedDay][timeWindow][scheduleColumn - 1])" />
+                  </div>
+                  <div class="schedule-content-item-title">
+                    {{ formattedEvents[selectedDay][timeWindow][scheduleColumn - 1].event_name }}
+                  </div>
+                </div>
               </div>
           </div>
         </div>
@@ -27,23 +34,33 @@
 import generalMixin from '../mixins/general';
 import Config from '../config/general';
 
+const startDate = new Date(Config.shared.START_DATE);
+const endDate = new Date(Config.shared.END_DATE);
+
+const eventBrandingTypes = [{ class: 'content-item-type-a', emptyStarImgName: 'star_purple_empty', filledStarImgName: 'star_purple_filled' }, { class: 'content-item-type-b', emptyStarImgName: 'star_white_empty', filledStarImgName: 'star_white_filled' }, { class: 'content-item-type-c', emptyStarImgName: 'star_white_empty', filledStarImgName: 'star_white_filled' }];
+
 export default {
   name: 'Schedule',
   mixins: [generalMixin],
   data() {
     return {
-      events: [],
-      selectedDay: 'Saturday',
-      days: ['Saturday', 'Sunday'],
+      rawEvents: [],
+      formattedEvents: {},
+      selectedDay: null,
+      days: [],
       timeWindows: [],
       scheduleColumns: 3,
+      dataLoaded: false,
     };
   },
   async mounted() {
     this.prepareTimeWindows();
+    this.populateDays();
     const env = this.getCurrentEnvironment();
-    this.events = await this.getData(Config[env].SCHEDULE_BASE_ENDPOINT, env, 'schedule');
-    console.log(this.events);
+    this.rawEvents = await this.getData(Config[env].SCHEDULE_BASE_ENDPOINT, env, 'schedule');
+    console.log(this.rawEvents);
+    this.processRawEvents();
+    this.dataLoaded = true;
     await this.activityTracking('SCHEDULE');
   },
   methods: {
@@ -52,6 +69,17 @@ export default {
     },
     selectTitleItem(day) {
       this.selectedDay = day;
+    },
+    populateDays() {
+      let currentDate = new Date(startDate.setDate(startDate.getDate() - 1));
+      console.log(currentDate)
+      while ((currentDate - endDate) !== 0) {
+        this.days.push(currentDate);
+        currentDate = new Date(currentDate.setDate(currentDate.getDate() + 1));
+      }
+      console.log(this.days)
+      // eslint-disable-next-line prefer-destructuring
+      this.selectedDay = this.days[0];
     },
     prepareTimeWindows() {
       this.timeWindows.push('12AM');
@@ -62,6 +90,34 @@ export default {
       for (let i = 1; i < 12; i += 1) {
         this.timeWindows.push(`${i}PM`);
       }
+    },
+    processRawEvents() {
+      for (let i = 0; i < this.rawEvents.length; i += 1) {
+        this.rawEvents[i].branding = eventBrandingTypes[i % 3];
+      }
+      this.days.forEach((day) => {
+        this.formattedEvents[day] = {};
+        this.timeWindows.forEach((timeWindow) => {
+          this.formattedEvents[day][timeWindow] = this.getEventsForTimeWindow(timeWindow, day);
+        });
+      });
+      console.log(this.formattedEvents);
+    },
+    convertTimeWindowTo24HourFormat(timeWindow) {
+      if (timeWindow.includes('AM')) {
+        return parseInt(timeWindow.replace(/[^0-9]/g, ''), 10);
+      }
+      return parseInt(timeWindow.replace(/[^0-9]/g, ''), 10) + 12;
+    },
+    getEventsForTimeWindow(timeWindow, day) {
+      return this.rawEvents.filter((rawEvent) => {
+        const rawEventStart = (new Date(rawEvent.start_time));
+        return rawEventStart.getHours() === this.convertTimeWindowTo24HourFormat(timeWindow) && rawEventStart.getDate() === day.getDate();
+      });
+    },
+    getImgUrl(event) {
+      const images = require.context('../assets', false, /\.png$/);
+      return images(`./${event.branding.emptyStarImgName}.png`);
     },
   },
 };
@@ -123,7 +179,7 @@ export default {
 }
 
 .timewindow {
-  border: 1px solid blue;
+  /* border: 1px solid blue; */
   height: 5vh;
   width: 100%;
 }
@@ -131,11 +187,50 @@ export default {
 .schedule-column {
   flex-grow: 1;
   flex-wrap: wrap;
+  margin-right: 1rem;
 }
 
 .schedule-content-item {
-  flex: 0 0 30%;
-  border: 1px solid green;
-  height: 100%;
+  height: 90%;
+  border-radius: 8px;
+  font-family: Noto Sans;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 16px;
+  line-height: 20px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  width: fit-content;
+}
+
+.content-item-type-a {
+  color: #B377DB;
+  background: #DED2E5;
+}
+
+.content-item-type-b {
+  background: #A88AA8;
+  border-radius: 8px;
+  color: #FFFFFF;
+}
+
+.content-item-type-c {
+  background: #B377DB;
+  border-radius: 8px;
+  color: #FFFFFF;
+}
+
+.schedule-content-item-star {
+  width: fit-content;
+  margin-left: .5rem;
+}
+
+.schedule-content-item-title {
+  flex-grow: 1;
+  text-align: start;
+  padding-left: 1rem;
+  margin-right: 2rem;
 }
 </style>
