e0f4ae55c836ff976ee5b55d509ec1e97c23a0d2
Hugo Burbelo <hburbelo@phone2action.com>
2020-10-04 18:23:59 -0400

    Customize FAQ

--- frontend/src/config/content.js
+++ frontend/src/config/content.js
@@ -33,61 +33,45 @@ export default {
     leftPanelItems: [
       {
         title: 'What is a "hackathon"?',
-        description: 'A hackathon is a technology-focused event where individuals and teams work on new projects in a short period of time. You can build anything you want. An app, a robot, a website. The only limit is your imagination!',
+        description: 'A hackathon is a programming competition in which competitors use technology to build innovative projects within 36 hours. There will be opportunities to win cool prizes, learn from our key-note speakers, and network with our sponsors.',
       },
       {
-        title: 'Why make an all-women and non-binary hackathon?',
-        description: '<b>Women, non-binary, and gender non-conforming people</b> are underrepresented in the tech world, and it’s up to everyone to change that! Technica’s goal is to create a welcoming environment where people from underrepresented genders can build cool projects and share their stories!',
+        title: 'Where will it be hosted?',
+        description: 'Hack the mountains will be hosted in October, 2020, completely virtually. All you need is to create a Slack account to communicate with our Hack the mountains community.',
       },
       {
-        title: 'Do I need to know how to code?',
-        description: '<b>No</b>, you don’t need ANY coding experience to participate! If you do, that’s great! If you don’t, that’s also great! Students of all skill levels are encouraged to attend, even if you’ve never written a line of code. We’re very beginner friendly and are prepared to teach you everything you need to learn right here. This year we are also introducing a beginners track to further help beginner coders start their tech journey!',
+        title: 'Are there any rules?',
+        description: 'Attendees must abide by the MLH Code of Conduct. We want to respect each other and keep Hack the mountains a safe environment for all participants.',
       },
       {
-        title: 'Where and when is Technica?',
-        description: 'Technica will be held <b>virtually</b> from <b>October 24th-25th, 2020</b> over a 24 hour period.',
+        title: 'Do I have to know how to code to join?',
+        description: 'Nope! You can learn along the way or team up with experienced programmers. Skills such as designing, business planning, and etc are all going to impact the projects in various ways!',
       },
       {
-        title: "Will this year's hackathon be different due to the COVID-19 pandemic?",
-        description: ' This year, we are still planning on offering all the same perks as previous years. However, we are going <b>completely virtual in order to follow coronavirus safety guidelines</b>. All communication throughout the event will be done on <b>Technica’s online platform, our Slack workspace, and Zoom video conferencing</b>. We will be sending out specifics of the working of the platform closer to the event.',
-      },
-      {
-        title: 'Who can attend Technica?',
-        description: 'Anyone who identifies as a woman, non-binary, or gender non-conforming person may register as a hacker! All ages are welcome. Additionally, all participants must follow the <a target="_blank" class="link" href="https://static.mlh.io/docs/mlh-code-of-conduct.pdf"><b>MLH code of conduct</b></a> and <a target="_blank" class="link" href="./files/terms.pdf"><b>Technica code of conduct</b></a>.',
-      },
-      {
-        title: 'How much does it cost to attend Technica?',
-        description: 'Technica is <b>FREE</b> for all participants, thanks to our amazing sponsors! Participation includes a lot of <b>free gear and swag</b>!',
+        title: 'Who can participate?',
+        description: 'Everyone can participate! We encourage anyone who is interested in learning more about Hackathons, programming, and the tech industry to join Hack the Mountains!',
       },
     ],
     rightPanelItems: [
       {
-        title: "What if I'm under 18?",
-        description: 'Under 18 and still want to code with us? Don’t worry! Technica is very minor friendly; we have accommodations for participants under 18!',
+        title: "How much does it cost?",
+        description: 'Hack the mountains is absolutely free to attend!',
       },
       {
-        title: 'What will I need?',
-        description: 'To complete the pre-registration process you will need a <b>valid student or government-issued ID card and your resume</b>. Since this year Technica will be hosted virtually, all you will need is your <b>hacking device (laptop, desktop, tablet, etc.) and Wi-Fi</b>!',
+        title: 'Can students attend?',
+        description: 'Yes, this is a student-held hackathon only. We would love to have your participation.',
       },
       {
-        title: 'How do teams work?',
-        description: 'You can work on a project on your own, or you can work with a team. Teams can be formed prior to the day of the hackathon with other participants, or at the team formation event before official hacking begins. <b>You can meet other hackers in the Technica Hacker Facebook group and Slack workspace.</b>Team size must not exceed four members, per MLH rules.',
+        title: 'What are the topics and prizes of the hackathon?',
+        description: "The topic and the prizes will be declared soon. Also once you've registered with an e-mail, we'll notify you via email about further announcements.",
       },
       {
-        title: 'How do I get swag?',
-        description: 'To get swag, submit a hack during Technica or attend at least 3 events, meetups, and/or workshops during Technica! At least one of those 3 events must be diversity-designated events, which will be labeled on our online platform.',
+        title: 'How many members in a team?',
+        description: 'A team consists of 2-4 members. You can also take part solo in this hackathon',
       },
       {
-        title: 'Who can I reach out to with questions or concerns?',
-        description: 'If you have any special needs you feel our organizers should be aware of or other questions or concerns, please <b>chat</b> with us in the bottom right-hand corner or email <a class="link" href="mailto:hello@gotechnica.org"><b>hello@gotechnica.org</b></a>.',
-      },
-      {
-        title: 'How can I register?',
-        description: 'Registration for Technica is now open! You can register <a class="link" href="https://gotechnica.typeform.com/to/TcQ4ecwG"><b>here</b></a>!',
-      },
-      {
-        title: 'Where and when is Technica?',
-        description: 'Technica will be held <b>virtually</b> from <b>October 24th-25th, 2020</b> over a 24 hour period.',
+        title: 'Can I volunteer as a mentor or judge?',
+        description: "Yes! Hack the Mountains wouldn't be possible without our volunteers. We will open mentor and judge sign ups soon, and you can get notified when it opens by signing up for email updates above!",
       },
     ],
   },
