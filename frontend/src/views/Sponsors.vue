db2dc1e7bcec32e6300629d2763b0858e799bb21
Hugo Burbelo <hburbelo@phone2action.com>
2020-10-03 21:25:37 -0400

    Add description to sponsors page

--- frontend/src/views/Sponsors.vue
+++ frontend/src/views/Sponsors.vue
@@ -3,6 +3,7 @@
     <h2 class="page-header">Sponsors</h2>
     <b-container class="sponsors-container">
       <div v-if="dataLoaded">
+        <p>Below is a list of our amazing event sponsors. Click a sponsor's logo below to visit their virtual booth, where you can view more information about their organization, schedule an appointment with them, and more!</p>
         <div v-for="section in sponsorTiers" :key="section" class="sponsor-section">
             <sponsor-section :tier="section" :sponsorList="sponsorsByTier[section]"/>
         </div>
